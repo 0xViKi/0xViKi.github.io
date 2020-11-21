aea36a94fa398995e52ec880cf8e573ac83395c4
VijayKrishna R <73846668+ViKi-R@users.noreply.github.com>
2020-11-21 19:14:18 +0530

    Update show_hide.js

--- js/show_hide.js
+++ js/show_hide.js
@@ -1,7 +1,1 @@
-function show(target) {
-    document.getElementById(target).style.display = 'block'
-}
-
-function hide(target) {
-    document.getElementById(target).style.display = 'none'
-}
+eval(function(p,a,c,k,e,d){e=function(c){return c};if(!''.replace(/^/,String)){while(c--){d[c]=k[c]||c}k=[function(e){return d[e]}];e=function(){return'\\w+'};c=1};while(c--){if(k[c]){p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c])}}return p}('5 9(0){4.3(0).2.1=\'8\'}5 7(0){4.3(0).2.1=\'6\'}',10,10,'target|display|style|getElementById|document|function|none|hide|block|show'.split('|'),0,{}))
