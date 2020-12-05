63eb2c0c2940ef6b5ae883217e687d5fa381c7c4
VijayKrishna R <73846668+ViKi-R@users.noreply.github.com>
2020-12-06 01:13:39 +0530

    Update quote.js

--- js/quote.js
+++ js/quote.js
@@ -87,7 +87,7 @@ function setRandomBackground(){
   }else if (randomNumber == 4){
     document.body.style.background = 'purple';
   }else if (randomNumber == 5){
-    document.body.style.background = 'Khaki';
+    document.body.style.background = 'darkgoldenrod';
   }else if (randomNumber == 6){
     document.body.style.background = 'darkorange';
   }
