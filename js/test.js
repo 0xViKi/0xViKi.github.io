39d81fa1ff5a57497856b2309981e2771c2764eb
VijayKrishna <73846668+0xViKi@users.noreply.github.com>
2024-08-27 00:22:11 +1000

    Delete js/test.js

--- js/test.js
+++
@@ -1,8 +0,0 @@
-// Get the document's cookies
-var cookies = document.cookie;
-
-// Log the cookies to the console (optional, for debugging)
-console.log("Cookies:", cookies);
-
-// Display an alert with the number 123
-alert(123);
